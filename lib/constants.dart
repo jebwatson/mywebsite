f031fb6dc20060bf56db63615125b7f6358dbc16
Jeb Watson <jeb.watson@gmail.com>
2021-07-26 19:44:01 -0500

    Get things looking ok for most web layouts

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+const EdgeInsetsGeometry standardContainerPadding = EdgeInsets.all(16.0);
+const EdgeInsetsGeometry standardButtonPadding = EdgeInsets.all(20.0);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:mywebsite/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,20 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: "Jeb Watson",
+      theme: ThemeData.dark().copyWith(
+        accentColor: Color.fromARGB(255, 69, 27, 111),
+        primaryColor: Color.fromARGB(255, 36, 30, 48),
+        backgroundColor: Color.fromARGB(255, 03, 03, 03),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'My Samples Portfolio'),
     );
   }
 }
@@ -46,68 +42,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final MediaQueryData queryData = MediaQuery.of(context);
+    final Size size = queryData.size;
+    final ThemeData theme = Theme.of(context);
+
+    return Container(
+      color: theme.backgroundColor,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: _buildHeader(theme, size),
+          ),
+          Expanded(
+            child: _buildBody(theme, size),
+            flex: 6,
+          ),
+          Expanded(
+            child: _buildFooter(theme, size),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget _buildHeader(ThemeData theme, Size size) {
+  return Container(
+    child: Padding(
+      padding: standardContainerPadding,
+      child: FittedBox(
+        fit: BoxFit.scaleDown,
+        alignment: Alignment.center,
+        child: AnimatedTextKit(
+          // Replace this with three distinct widgets that wait on each other and replace the with static text once the animation is complete
+          animatedTexts: [
+            TypewriterAnimatedText(
+              "Welcome, my name's Jeb",
+              speed: const Duration(milliseconds: 60),
+              textStyle: theme.textTheme.headline3,
+              textAlign: TextAlign.center,
+            ),
+          ],
+          isRepeatingAnimation: false,
+        ),
+      ),
+    ),
+    color: theme.backgroundColor,
+  );
+}
+
+Widget _buildBody(ThemeData theme, Size size) {
+  return Container(
+    child: Center(
+      child: Text(
+        "App Samples Go Here",
+        style: theme.textTheme.headline6,
+      ),
+    ),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+      color: theme.primaryColor,
+    ),
+  );
+}
+
+Widget _buildFooter(ThemeData theme, Size size) {
+  return Container(
+    child: Center(
+      child: TextButton(
+        child: const Text("Contact Me"),
+        onPressed: () => {print("You pressed the button")},
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+          primary: Colors.white,
+          backgroundColor: theme.accentColor,
+          textStyle: theme.textTheme.bodyText1,
+        ),
+      ),
+    ),
+    color: theme.primaryColor,
+  );
 }
